f5969d34ded3b0eb1f1dc08bc54dc9122c86a768
Dibyajyoti Panda <dbjpanda@live.com>
2018-06-05 09:14:35 +0530

    Update drupal-build.sh

--- travis/drupal-build.sh
+++ travis/drupal-build.sh
@@ -2,7 +2,7 @@
 
 docker exec -it drupal.localhost.php composer install
 
-if curl -H Host:drupal.localhost http://127.0.0.1 | grep -q "Drupal " ; then
+if curl -L -H Host:drupal.localhost http://127.0.0.1 | grep -q "Drupal " ; then
   echo "Drupal has been successfully built up"
   exit 0
 else
